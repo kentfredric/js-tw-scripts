77b2e13205e4109dfeac51ee1c214fd7c5a3c64a
Kent Fredric <kentfredric@gmail.com>
2013-08-02 02:59:29 +1200

    Fix up weird thingy

--- offensive_max.js
+++ offensive_max.js
@@ -66,7 +66,7 @@ function smart_offensive() {
 function recruit_offensive() {
   var troops = smart_offensive();
   $("#axe_0").val(troops.axes);
-  $("light_0").val(troops.lc);
+  $("#light_0").val(troops.lc);
   $("input[type=submit]").click();
   return false;
 }
